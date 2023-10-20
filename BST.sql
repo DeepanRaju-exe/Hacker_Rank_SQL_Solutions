5c9d01bb6719ef636da2d52c319fdd9e2982e6f9
DeepanRaju <sgdeepan25@gmail.com>
2023-10-20 19:31:13 +0530

    update

--- BST.sql
+++ BST.sql
@@ -1,7 +1,7 @@
 WITH cte AS 
 (SELECT DISTINCT N1 AS InnerNode 
  FROM 
-(SELECT a.n as N1,a.p as P1 ,b.n AS N2, b.p AS P2, c.n AS N1ispar FROM BST a
+(SELECT a.n as N1 FROM BST a
 JOIN BST b ON a.p = b.n
 JOIN BST C ON c.p =a.n)x
 )
@@ -12,3 +12,13 @@ WHEN N NOT IN (SELECT InnerNode from cte) AND p IS NOT NULL THEN "Leaf"
 ELSE "Root" END AS Node_type
 FROM BST 
 ORDER BY n ASC ;
+
+###################################
+#Another approach is simplier but its via sub query 
+#1. for every node we are checking the how many node has it as parent and we select only those are having 1 or more
+#2. Becuase those nodes are inner nodes
+
+select N,
+       if(P is null, 'Root', if((select count(*) from BST where P = B.N)> 0, 'Inner', 'Leaf')) 
+from BST as B 
+order by N;
